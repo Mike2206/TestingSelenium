cf99b04e1c61715e839e93e92fd01c05c476ffb7
Mike2206 <m.youzwiak@gmail.com>
2019-07-20 13:22:13 +0200

    testing

--- src/test/java/com/mike/selenium/TestScenario1.java
+++ src/test/java/com/mike/selenium/TestScenario1.java
@@ -1,4 +1,20 @@
 package com.mike.selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
 public class TestScenario1 {
+
+    @BeforeSuite
+    public void startWebdriver() {
+        System.setProperty("webdriver.gecko.driver", "C:\\WebDrivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+    }
+
 }
