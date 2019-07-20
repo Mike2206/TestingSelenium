af4d64838e5d77c1a39f37ad351bfc1972f08f37
Mike2206 <m.youzwiak@gmail.com>
2019-07-20 14:16:02 +0200

    testinggg

--- src/test/java/com/mike/selenium/TestScenario1.java
+++ src/test/java/com/mike/selenium/TestScenario1.java
@@ -2,19 +2,36 @@ package com.mike.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestScenario1 {
+    //Variables
+    public String url;
+    WebDriver driver = new FirefoxDriver();
 
     @BeforeSuite
     public void startWebdriver() {
+        url = "http://automationpractice.com/index.php?";
         System.setProperty("webdriver.gecko.driver", "C:\\WebDrivers\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-        driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
 
+    @AfterSuite
+    public void tearDown(){
+        driver.manage().deleteAllCookies();
+        driver.close();
+    }
+
+    @Test(priority = 1)
+    public void Test1(){
+        driver.navigate().to("url");
+        Assert.assertTrue(driver.getTitle().contains("My Store"));
     }
 
 }
